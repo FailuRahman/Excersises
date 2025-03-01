fdde6eaf012bfd3601368a913905329d2a8b890a
FailuRahman <failuzz00@gmail.com>
2025-03-01 11:00:00 +0530

    Ado

---
+++ react4/myapp3/src/Component/Test1.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Test from './Test'
+
+function Test1() {
+  return (
+    <div>
+        <Test/>
+         one
+        
+    </div>
+  )
+}
+
+export default Test1

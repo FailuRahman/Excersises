4cfbab0ddd85d69f4085cf824bdaf7c6c4b0ee9d
FailuRahman <failuzz00@gmail.com>
2025-03-22 11:00:00 +0530

    react5

---
+++ React5/myapp5/src/Component/MainRouter.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+
+
+
+function MainRouter() {
+  return (
+    <div>
+        <ul>
+            <li>
+                <Link to>Home</Link>
+            </li>
+            <li>
+              
+            </li>
+        </ul>
+        
+    </div>
+  )
+}
+
+export default MainRouter

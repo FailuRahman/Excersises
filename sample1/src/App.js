13bc9bb751d41e06f2ff1a058c8e9f4d34cbc8a2
FailuRahman <failuzz00@gmail.com>
2025-04-04 11:00:00 +0530

    sample

---
+++ sample1/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+//import MainRouter from './Compnents/MainRouter';
+import Reducer from './Compnents/Pages/Reducer';
+
+function App() {
+  return (
+    <div>
+      <Reducer/>
+    {/* <MainRouter/> */}
+    </div>
+  );
+}
+
+export default App;
